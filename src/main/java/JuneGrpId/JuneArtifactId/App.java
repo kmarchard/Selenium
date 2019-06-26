0d3854531a5fa25275c08de6de0e82c7ee1eda9a
bhoomi <bhoomi@LAPTOP-7RM6SAN7>
2019-06-26 08:18:53 +0530

    git commit -m "git init"

---
+++ src/main/java/JuneGrpId/JuneArtifactId/App.java
@@ -0,0 +1,13 @@
+package JuneGrpId.JuneArtifactId;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/JuneGrpId/JuneArtifactId/TC1.java
@@ -0,0 +1,10 @@
+package JuneGrpId.JuneArtifactId;
+
+import org.junit.Test;
+
+public class TC1 {
+@Test
+public void test1(){
+	System.out.println("First Test case");
+}
+}
